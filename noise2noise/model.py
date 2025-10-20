9dff75f1c2fc4050a5764936f1ae60c619cfcfd8
Chen <zc13@emimdgxa100gpu7.partners.org>
2025-10-20 09:56:03 +0000

    change sampler

--- noise2noise/model.py
+++ noise2noise/model.py
@@ -745,8 +745,8 @@ class Sampler(object):
         dl = DataLoader(self.generator, batch_size = self.batch_size, shuffle = False, pin_memory = True, num_workers = 0)# cpu_count())
         self.histogram_equalization = self.generator.histogram_equalization
         print('histogram equalization: ', self.histogram_equalization)
-        self.bins = np.load('/mnt/camca_NAS/denoising/Data/histogram_equalization/bins.npy')
-        self.bins_mapped = np.load('/mnt/camca_NAS/denoising/Data/histogram_equalization/bins_mapped.npy')        
+        self.bins = generator.bins
+        self.bins_mapped = generator.bins_mapped      
         self.background_cutoff = self.generator.background_cutoff
         self.maximum_cutoff = self.generator.maximum_cutoff
         self.normalize_factor = self.generator.normalize_factor
@@ -781,7 +781,7 @@ class Sampler(object):
 
         # start to run
         with torch.inference_mode():
-            print('gt_img shape: ', gt_img.shape)
+            print('reference_img shape: ', gt_img.shape)
             for z_slice in range(0,gt_img.shape[-1]):
                 batch_input, batch_gt = next(self.cycle_dl)
                 data_input = batch_input.to(device)
